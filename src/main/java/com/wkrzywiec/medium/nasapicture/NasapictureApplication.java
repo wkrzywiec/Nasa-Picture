aa2d25e0cefaf6c6f98f7b0dcd7594ebd858722e
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2019-03-20 06:17:36 +0100

    Init commit

---
+++ src/main/java/com/wkrzywiec/medium/nasapicture/NasapictureApplication.java
@@ -0,0 +1,13 @@
+package com.wkrzywiec.medium.nasapicture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasapictureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasapictureApplication.class, args);
+	}
+
+}
