26c3d54b49ef336ba939528d15116f00914cf60d
Wojciech Krzywiec <wojciechkrzywiec@gmail.com>
2019-03-23 13:39:16 +0100

    Get data from Astronomy Picture Of a Day service

--- src/main/java/com/wkrzywiec/medium/nasapicture/NasapictureApplication.java
+++ src/main/java/com/wkrzywiec/medium/nasapicture/NasapictureApplication.java
@@ -1,13 +1,23 @@
 package com.wkrzywiec.medium.nasapicture;
 
+import com.wkrzywiec.medium.nasapicture.model.AstronomyPictureOfDay;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class NasapictureApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(NasapictureApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(NasapictureApplication.class, args);
+
+		RestTemplate restTemplate = new RestTemplate();
+		AstronomyPictureOfDay response = restTemplate.getForObject("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&hd", AstronomyPictureOfDay.class);
+		log.info(response.toString());
 	}
 
 }
---
+++ src/main/java/com/wkrzywiec/medium/nasapicture/model/AstronomyPictureOfDay.java
@@ -0,0 +1,34 @@
+package com.wkrzywiec.medium.nasapicture.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class AstronomyPictureOfDay {
+
+    @JsonProperty("copyright")
+    private String copyright;
+
+    @JsonProperty("date")
+    private String date;
+
+    @JsonProperty("explanation")
+    private String explanation;
+
+    @JsonProperty("hdurl")
+    private String hdurl;
+
+    @JsonProperty("media_type")
+    private String mediaType;
+
+    @JsonProperty("service_version")
+    private String serviceVersion;
+
+    @JsonProperty("title")
+    private String title;
+
+    @JsonProperty("url")
+    private String url;
+}
